4aaf5c905c3c7d9293d756cf9e046b623e6ae8a5
anknits <ankuragarwall04@gmail.com>
2016-10-01 20:42:14 +0000

    Request Header Parser

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var useragent = require('express-useragent');
 
+app.use(useragent.express());
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    var source = req.useragent.source
+    var software = source.substring(source.indexOf("(") + 1, source.indexOf(")"))
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    var lang = req.headers["accept-language"]
+    var language = lang.substring(0, lang.indexOf(","))
+    res.send(JSON.stringify({"ipadress": ip , "language": language, "software": software}));
+    //console.log(req.useragent, req.ip, req.headers)
 });
 
 app.listen(8080, function () {
